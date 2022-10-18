c02034b687e8172b2e56f802a3086ac6e06a47ac
rabiademirr <75272533+rabiademirr@users.noreply.github.com>
2022-10-18 22:44:50 +0300

    request header middleware added

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -43,7 +43,9 @@ class Kernel extends HttpKernel
              \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             //'throttle:5,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\ApiLogger::class
+            \App\Http\Middleware\ApiLogger::class,
+            \App\Http\Middleware\HttpHeader::class
+
         ],
     ];
 
